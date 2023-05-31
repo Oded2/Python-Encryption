2b36358736186062b10b5ac1f653e2259874beaf
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-05-31 23:50:41 +0300

    Works on only letters, no spaces

--- secretKey.py
+++ secretKey.py
@@ -1,7 +1,3 @@
-
-
-# az_map = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13,
-#           'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26}
 az_map = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12,
           'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25}
 
@@ -9,7 +5,7 @@ az_map = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8
 def hash(string):
     final = 0
     for i in string:
-        final += az_map[i.lower()]
+        final += az_map[i.lower()] + 1
 
     return final
 
@@ -70,8 +66,12 @@ def decrypt(string, code):
     return final
 
 
-code = 123456789
-a = encrypt("abcdefghijklmnopqrstuvwxyz", code)
-print(f"a: {a}")
-b = decrypt(a, code)
-print(f"b: {b}")
+text = input("What's the text? ")
+code = hash(input("What's your password? "))
+print(f"Hash: {code}")
+encrypted = encrypt(text, code)
+print(f"Encrypted text: {encrypted}")
+code = hash(input("What's the password? "))
+print(f"Hash: {code}")
+decrypted = decrypt(encrypted, code)
+print(f"Decrypted text: {decrypted}")
